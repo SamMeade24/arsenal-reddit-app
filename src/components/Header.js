2b0cc1fc5ad74a28b0e95d03871f4991f75f0ed3
Sam Meade <sammeade1990@outlook.com>
2026-01-16 16:42:43 +0000

    Added Header component and integrated into App.js

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <h1>Arsenal Reddit Comment Explorer</h1>
+            <p>Hear the latest news and explore discussions from fellow Arsenal supporters and media people</p>
+        </header>
+    );
+}
+
+export default Header;
