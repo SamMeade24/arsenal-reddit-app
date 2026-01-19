db6477f024afec2b05b7ac3a961c93d3c80c7000
Sam Meade <sammeade1990@outlook.com>
2026-01-19 15:29:59 +0000

    Added the Comment and CommentList component scaffolding

---
+++ src/components/Comment.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Comment() {
+    return (
+        <div>Single comment will go here</div>;
+    )
+}
+
+export default Comment;
---
+++ src/components/CommentList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CommentList() {
+    return (
+        <div>Comments will go here</div>;
+    )
+}
+
+export default CommentList;
