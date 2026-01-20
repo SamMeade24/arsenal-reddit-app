21b2470c96ea4df6ede73f3d7e8733bbb8a38309
Sam Meade <sammeade1990@outlook.com>
2026-01-20 13:32:14 +0000

    Added ID fields to dummy post data

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   const [hasError, setHasError] = useState(false);
   
   const dummyPosts = [
-    {title: "Advantage Arsenal as they beat Chelsea 3-2", author: "Fabrizio Romano", score: 34}, 
-    {title: "Attention turns to Forest away on Saturday", author: "David Ornstein", score: 12}
+    {id: 1, title: "Advantage Arsenal as they beat Chelsea 3-2", author: "Fabrizio Romano", score: 34}, 
+    {id: 2, title: "Attention turns to Forest away on Saturday", author: "David Ornstein", score: 12}
   ];
 
   if (isLoading) {
